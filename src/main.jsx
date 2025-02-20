bb5a6d84048718a88eb9bc3525491037470792e2
= <augustodemeloh@protonmail.com>
2025-02-19 21:57:48 -0300

    fix: move axios interceptor declaration to entry file

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,13 @@ window.axios = axios
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 window.axios.defaults.withCredentials = true
 window.axios.defaults.baseURL = 'http://car-parking-api.test/api/v1'
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401) logout(true)
+    return Promise.reject(error)
+  },
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
